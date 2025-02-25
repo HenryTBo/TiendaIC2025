be9b8dbdf8f6bfdc1fea3ba1dce6392e3de415a5
Henry Trejos <133723568+HenryTBo@users.noreply.github.com>
2025-02-24 19:51:48 -0600

    Semana 6

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -1,4 +1,3 @@
-
 package com.tienda.domain;
 
 import jakarta.persistence.*;
@@ -6,24 +5,31 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
+@Entity
+@Table(name ="Categoria")
 
-@Table(Name="Categoria")
-public class Categoria implements Serializable{
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Columna(name="id_categoria")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
     private long idCategoria;
+
     private String descripcion;
-    @Columna(name="ruta_imagen")
+    @Column(name = "ruta_imagen")
     private String rutaImagen;
     private boolean activo;
-  
-    public Categoria(){
+
+    public Categoria() {
     }
-    
-    public Categoria(String descripcion,boolean activo){
-        this.descripcion = descriipcion;
+
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
         this.activo = activo;
     }
+    
+    
+
 }
