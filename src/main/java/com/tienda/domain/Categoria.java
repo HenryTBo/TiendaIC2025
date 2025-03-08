6c893c8032495fc246b6295cd9dd2bda47ce5e85
gabot <gabot@HenryPC>
2025-03-08 00:12:53 -0600

    Semana 8

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -3,6 +3,7 @@ package com.tienda.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @Entity
@@ -22,6 +23,10 @@ public class Categoria implements Serializable {
     private String rutaImagen;
     private boolean activo;
 
+    @OneToMany
+    @JoinColumn(name="id_categoria",updatable=false)
+    List<Producto>productos;
+    
     public Categoria() {
     }
 
