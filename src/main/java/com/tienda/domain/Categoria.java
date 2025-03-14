ab182136712bf12952d7b6c65f70d2946cfc3fcf
gabot <gabot@HenryPC>
2025-03-14 11:09:49 -0600

    semana 9

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 @Data
 @Entity
-@Table(name ="Categoria")
+@Table(name = "Categoria")
 
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
@@ -24,9 +24,9 @@ public class Categoria implements Serializable {
     private boolean activo;
 
     @OneToMany
-    @JoinColumn(name="id_categoria",updatable=false)
-    List<Producto>productos;
-    
+    @JoinColumn(name = "id_categoria", updatable = false)
+    List<Producto> productos;
+
     public Categoria() {
     }
 
@@ -34,7 +34,45 @@ public class Categoria implements Serializable {
         this.descripcion = descripcion;
         this.activo = activo;
     }
-    
-    
+
+    public long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 
 }
